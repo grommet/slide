ba0162aa89eff27ad469da6ab4bfba4b592b66fc
Eric Soderberg <eric.soderberg@hpe.com>
2019-09-30 08:08:14 +0200

    Always justify content

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,19 @@
-import { Box, Markdown, Paragraph } from 'grommet'
-import React from 'react'
+import { Box, Markdown, Paragraph } from 'grommet';
+import React from 'react';
 
 const LightBox = props => (
   <Box
-    pad={{ horizontal: "xlarge" }}
+    pad={{ horizontal: 'xlarge' }}
     background={{ color: 'dark-3', opacity: 'medium' }}
     justify="center"
     round="small"
     {...props}
   />
-)
+);
 
 const Content = ({ image, index, slide }) => {
-  const size = (slide.length < 10) ? 'xlarge' : 'large'
-  const textAlign = (slide.indexOf("\n") === -1) ? 'center' : 'start'
+  const size = slide.length < 10 ? 'xlarge' : 'large';
+  const textAlign = slide.indexOf('\n') === -1 ? 'center' : 'start';
   const components = {
     h1: { props: { textAlign, size } },
     h2: { props: { textAlign, size } },
@@ -22,25 +22,25 @@ const Content = ({ image, index, slide }) => {
     ul: { component: Box, props: { as: 'ul', margin: { left: 'medium' } } },
     ol: { component: Box, props: { as: 'ol', margin: { left: 'medium' } } },
     li: { component: Paragraph, props: { as: 'li', size } },
-  }
+  };
 
   // if second line of slide is an image, make it the background,
   // and remove from markdown content
-  const lines = slide.split("\n")
-  const secondLine = lines[1] || ''
-  const match = secondLine.match(/^!\[.*\]\((.+)\)$/)
-  let content = slide
-  let background = image || `accent-${(index % 3) + 1}`
+  const lines = slide.split('\n');
+  const secondLine = lines[1] || '';
+  const match = secondLine.match(/^!\[.*\]\((.+)\)$/);
+  let content = slide;
+  let background = image || `accent-${(index % 3) + 1}`;
   if (match) {
-    background = `url(${match[1]})`
-    lines.splice(1, 1)
-    content = lines.join("\n")
+    background = `url(${match[1]})`;
+    lines.splice(1, 1);
+    content = lines.join('\n');
   }
-  content = <Markdown components={components}>{content}</Markdown>
+  content = <Markdown components={components}>{content}</Markdown>;
 
-  const backgroundImage = (background.slice(0, 4) === 'url(')
+  const backgroundImage = background.slice(0, 4) === 'url(';
   if (backgroundImage) {
-    content = <LightBox>{content}</LightBox>
+    content = <LightBox>{content}</LightBox>;
   }
 
   return (
@@ -48,12 +48,12 @@ const Content = ({ image, index, slide }) => {
       fill
       pad="xlarge"
       background={background}
-      justify={backgroundImage ? 'center' : undefined}
+      justify="center"
       align={backgroundImage ? 'center' : undefined}
     >
       {content}
     </Box>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
--- src/Editor.js
+++ src/Editor.js
@@ -1,20 +1,17 @@
-import {
-  Box, Button, TextArea, TextInput,
-} from 'grommet'
-import { Apps, Share as ShareIcon } from 'grommet-icons'
-import React from 'react'
-import Manage from './Manage'
-import Share from './Share'
-import { characterToSlideIndex } from './slide'
+import { Box, Button, TextArea, TextInput } from 'grommet';
+import { Apps, Share as ShareIcon } from 'grommet-icons';
+import React from 'react';
+import Manage from './Manage';
+import Share from './Share';
+import { characterToSlideIndex } from './slide';
 
 const Editor = ({ set, onChange, setCurrent }) => {
-  const [manage, setManage] = React.useState()
-  const [share, setShare] = React.useState()
+  const [manage, setManage] = React.useState();
+  const [share, setShare] = React.useState();
 
-  const checkCaret = (node) => {
-    console.log('!!! check', node)
-    setCurrent(characterToSlideIndex(set.text, node.selectionStart))
-  }
+  const checkCaret = node => {
+    setCurrent(characterToSlideIndex(set.text, node.selectionStart));
+  };
 
   return (
     <Box basis="medium">
@@ -26,18 +23,18 @@ const Editor = ({ set, onChange, setCurrent }) => {
         />
         <TextInput
           value={set.name || ''}
-          onChange={(event) => {
-            const nextSet = JSON.parse(JSON.stringify(set))
-            nextSet.name = event.target.value
-            onChange(nextSet)
+          onChange={event => {
+            const nextSet = JSON.parse(JSON.stringify(set));
+            nextSet.name = event.target.value;
+            onChange(nextSet);
           }}
           onBlur={() => {
             // ensure we have the name in our list
-            const stored = window.localStorage.getItem('slide-sets')
-            const sets = stored ? JSON.parse(stored) : []
+            const stored = window.localStorage.getItem('slide-sets');
+            const sets = stored ? JSON.parse(stored) : [];
             if (sets.indexOf(set.name) === -1) {
-              sets.unshift(set.name)
-              window.localStorage.setItem('slide-sets', JSON.stringify(sets))
+              sets.unshift(set.name);
+              window.localStorage.setItem('slide-sets', JSON.stringify(sets));
             }
           }}
         />
@@ -50,22 +47,22 @@ const Editor = ({ set, onChange, setCurrent }) => {
       <TextArea
         fill
         value={set.text}
-        onChange={(event) => {
-          const nextSet = JSON.parse(JSON.stringify(set))
-          nextSet.text = event.target.value
-          onChange(nextSet)
+        onChange={event => {
+          const nextSet = JSON.parse(JSON.stringify(set));
+          nextSet.text = event.target.value;
+          onChange(nextSet);
         }}
-        onKeyDown={(event) => checkCaret(event.target)}
-        onClick={(event) => checkCaret(event.target)}
+        onKeyDown={event => checkCaret(event.target)}
+        onClick={event => checkCaret(event.target)}
       />
       <Box flex={false}>
         <TextInput
           placeholder="published theme"
           value={set.theme || ''}
-          onChange={(event) => {
-            const nextSet = JSON.parse(JSON.stringify(set))
-            nextSet.theme = event.target.value
-            onChange(nextSet)
+          onChange={event => {
+            const nextSet = JSON.parse(JSON.stringify(set));
+            nextSet.theme = event.target.value;
+            onChange(nextSet);
           }}
         />
       </Box>
@@ -74,7 +71,7 @@ const Editor = ({ set, onChange, setCurrent }) => {
         <Share set={set} onChange={onChange} onClose={() => setShare(false)} />
       )}
     </Box>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
